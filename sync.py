904c71d84030765800de5cc67ab632ea679d5ad0
Martijn Kint <tunaman@gmail.com>
2023-12-05 16:18:41 +0100

    Allow multiple domains

--- sync.py
+++ sync.py
@@ -10,27 +10,30 @@ def main():
     config.load_incluster_config()
     api = client.CustomObjectsApi()
 
-    custom_domain = os.environ.get('WHITELIST_CUSTOM_DOMAIN')
+    # Get a comma-separated list of domains from the environment variable
+    custom_domains = os.environ.get('ALLOW_LIST_DOMAINS')
+    domains = custom_domains.split(',') if custom_domains else []
 
-    current = []
-    public_ip = urlopen('https://api.ipify.org').read().decode('utf8')
-    current.append(public_ip)
+    current_ips = []
+    public_ips = urlopen('https://api.ipify.org').read().decode('utf8')
+    current_ips.append(public_ips)
 
-    if custom_domain:
-        ip_list = socket.gethostbyname_ex(custom_domain)[2]
-        for ip in ip_list:
-            current.append(ip)
+    for domain in domains:
+        if domain:
+            ip_list = socket.gethostbyname_ex(domain)[2]
+            for ip in ip_list:
+                current_ips.append(ip)
 
     patch_body = {
         "spec": {
             "ipWhiteList": {
-                "sourceRange": current
+                "sourceRange": current_ips
             }
         }
     }
 
-    name = os.environ.get('WHITELIST_MIDDLEWARE_NAME', 'ip-whitelist')
-    namespace = os.environ.get('WHITELIST_TRAEFIK_NAMESPACE', 'traefik-system')
+    name = os.environ.get('ALLOW_LIST_MIDDLEWARE_NAME', 'ip-allowlist')
+    namespace = os.environ.get('ALLOW_LIST_TRAEFIK_NAMESPACE', 'traefik-system')
 
     patch_resource = api.patch_namespaced_custom_object(
         group="traefik.containo.us",
